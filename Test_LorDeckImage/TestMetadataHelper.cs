bf3c0b56f9cc33d071965503c6a0c1dc79b5f8c6
Raikeit <k.arai.nut@gmail.com>
2022-12-18 20:22:05 +0900

    add: Metadataのファクトリーメソッドのテスト追加

--- Test_LorDeckImage/TestMetadataHelper.cs
+++ Test_LorDeckImage/TestMetadataHelper.cs
@@ -15,5 +15,14 @@ namespace Test_LorDeckImage
             Assert.True(Directory.Exists(corePath));
             Assert.True(Directory.Exists(set6cdePath));
         }
-    }
+
+        [Fact]
+        public void TestMetadataFactory()
+        {
+            Metadata metadataEnUs = MetadataHelper.GetMetadataUrl("en_us");
+            Metadata metadataJaJp = MetadataHelper.GetMetadataUrl("ja_jp");
+
+            Assert.Equal("en_us", metadataEnUs.Locale);
+            Assert.Equal("ja_jp", metadataJaJp.Locale);
+        }
 }
